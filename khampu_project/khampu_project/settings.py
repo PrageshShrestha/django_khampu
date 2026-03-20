707d89bffba9884ad60e83e63a4c05b30fdcf799
Pragesh <pragesh@192.168.254.10>
2026-03-20 08:27:13 +0545

    success:deployed in render

--- khampu_project/khampu_project/settings.py
+++ khampu_project/khampu_project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x(x&c-n$aue%oi!^^vb7#=5#*9q9(ok48-ug2y6nbo(*yi7h19'
 
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "Render"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME') , "127.0.0.1"]
 
 
 # Application definition
